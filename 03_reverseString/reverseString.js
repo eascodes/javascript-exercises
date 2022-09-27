01b3f1ccafd798eba2b7c822503d7f08b3110f36
Emily <easchoettler@gmail.com>
2022-09-27 15:12:53 -0400

    Add solution to exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr = str.split('');
+    let reversed = splitStr.reverse();
+    return reversed.join('');
 };
 
 // Do not edit below this line
