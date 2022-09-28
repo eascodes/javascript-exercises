b65c3c7a60176fa30c77a946e0ff6858478a5797
Emily <easchoettler@gmail.com>
2022-09-28 09:00:05 -0400

    Add solution to exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
 
+const sumAll = function(x,y) {
+
+let answer = 0;
+
+if (x >= 0 && y >= 0 && Number.isInteger(x) && Number.isInteger(y)) {
+    if (x < y) {
+        for (i = x; i <= y; i++) {
+            answer += i;
+        }
+    } else if (x > y) {
+        for (i = y; i <= x; i++) {
+            answer += i;
+        }
+    }
+} else {return "ERROR";}
+
+return answer;
 };
 
 // Do not edit below this line
