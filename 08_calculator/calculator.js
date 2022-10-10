d4b58651bae8131485c14e7b145ce82fb28d03aa
Emily <easchoettler@gmail.com>
2022-10-10 14:02:40 -0400

    Add solution to exericise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	const total = numArr.reduce((total, num) => {
+    return total + num;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  const total = numArr.reduce((total, num) => {
+    return total * num;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function factorialize(num) {
+	if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else {
+    return (num * factorialize(num - 1));
+  }
 };
 
 // Do not edit below this line
