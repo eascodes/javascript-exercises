9d7590f20fd73a634a632b75f706b6f106f3eaf2
Emily <easchoettler@gmail.com>
2022-10-11 09:05:29 -0400

    Add solution to exercise 10_fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function palCheck(str) {
+    const arr = str
+        .toLowerCase()
+        .split("")
+        .filter(letter => /^[a-zA-Z]+$/.test(letter));
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == arr.reverse()[i]) {
+            continue;
+        } else {return false;}
+    } return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function fib(num) {
+    if (num < 0) return "OOPS"; else 
+    if (num == 0) return 0;
 
+    let arr = [1, 1];
+    for (let i = 0; i < num; i++) {
+        arr.push(arr[i] + (arr[i+1]));
+    }
+    return arr[num - 1];
 };
+
+//create a fibonacci sequence in an array
+//identify number based on index in the fib array
 
 // Do not edit below this line
 module.exports = fibonacci;
